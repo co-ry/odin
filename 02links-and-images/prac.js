1758842433b993027b2791d885f8143b61b4bb44
Cory <co.ry@icloud.com>
2023-09-17 12:06:52 -0400

    added odin project folders and some js | rps bones

---
+++ 02links-and-images/prac.js
@@ -0,0 +1,18 @@
+console.log((4+6+9)/77)
+console.log("hello world")
+
+
+let a = 10
+
+9 * a
+console.log(a)
+
+let b = 7 * a
+
+console.log(a)
+console.log(b)
+
+let apples = "2";
+let oranges = "3";
+
+alert( +apples + +oranges );
